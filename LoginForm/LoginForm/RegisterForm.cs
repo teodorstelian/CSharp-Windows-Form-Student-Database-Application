a83ffa57b5bc1122b78ce0e9bcfa5c3e792e313a
Alexandru Ciobotaru <ciobotaru_alex@yahoo.com>
2020-05-09 16:35:22 +0300

    New register

--- LoginForm/LoginForm/RegisterForm.cs
+++ LoginForm/LoginForm/RegisterForm.cs
@@ -7,15 +7,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
+
 
 namespace LoginForm
 {
 	public partial class RegisterForm : Form
 	{
+		SqlConnection myCon = new SqlConnection();
+		DataSet dsUsersNew;
+
 		public RegisterForm()
 		{
+			myCon = new SqlConnection();
 			InitializeComponent();
 		}
+
+		public DataSet getUsers()
+		{
+
+			dsUsersNew = new DataSet();
+
+			SqlDataAdapter daUsers = new SqlDataAdapter("SELECT * FROM Users", myCon);
+			daUsers.Fill(dsUsersNew, "Users");
+
+			myCon.Close();
+			return dsUsersNew;
+		}
+
+		private int InsertIntoUsers(String username, String email, String password)
+		{
+			int i = 0;
+			myCon.ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\StudentHelperDatabase.mdf;Integrated Security=True";
+
+			string query = "INSERT into Users(username, email, password) values('" + usernameRegister.Text + "', '" + emailRegister.Text + "', '" + passwordRegister.Text + "')";
+
+			using (SqlCommand command = new SqlCommand(query, myCon))
+			{
+				myCon.Open();
+				i = command.ExecuteNonQuery();
+			}
+			return i;
+		}
+
+
+		private void register_Click(object sender, EventArgs e)
+		{
+
+			string username = usernameRegister.Text;
+			string email = emailRegister.Text;
+			string password = passwordRegister.Text;
+
+			InsertIntoUsers(username, email, password);
+
+			clearInputs();
+
+			dsUsersNew = getUsers();
+
+			myCon.Close();
+		}
+
+		private void clearInputs()
+		{
+			usernameRegister.Clear();
+			emailRegister.Clear();
+			passwordRegister.Clear();
+
+		}
+
+
 
 		/// <summary>
 		///  Metoda care schimba culoarea textului din textboxul username si a panelului de dedesupt.
@@ -25,7 +85,7 @@ namespace LoginForm
 		private void username_Click(object sender, EventArgs e)
 		{
 			usernameRegister.Clear();
-			
+
 			userBarRegister.BackColor = Color.FromArgb(3, 174, 218);
 			usernameRegister.ForeColor = Color.FromArgb(3, 174, 218);
 
@@ -75,13 +135,7 @@ namespace LoginForm
 			emailRegister.ForeColor = Color.FromArgb(3, 174, 218);
 		}
 
-		private void register_Click(object sender, EventArgs e)
-		{
-			MainApp User = new MainApp();
-			this.Hide();
-			User.ShowDialog();
-			this.Close();
-		}
+
 
 		private void mainMenu_Click(object sender, EventArgs e)
 		{
@@ -90,5 +144,10 @@ namespace LoginForm
 			Menu.ShowDialog();
 			this.Close();
 		}
+
+
+
+
+
 	}
 }
